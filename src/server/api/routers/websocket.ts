3faaab697980242e6a45f1cc427138f43c159086
Jan Poonthong <janpoonthong628@gmail.com>
2023-12-27 20:51:22 +0700

    feat: connect to websocket and emit data to websocket server

--- src/server/api/routers/websocket.ts
+++ src/server/api/routers/websocket.ts
@@ -2,9 +2,47 @@ import {
   createTRPCRouter,
   protectedProcedure,
 } from "~/server/api/trpc";
+import io from 'socket.io-client';
+import { resolve } from "path";
+
+const SOCKET_SERVER_URL = 'http://localhost:3005';
 
 export const websocketRouter = createTRPCRouter({
 
-  greeting: protectedProcedure.query(() => 'hello tRPC v10!'),
+  greeting: protectedProcedure.query(async () => {
+
+    const data = "Someone open the door";
+
+    try {
+      const socket = io(SOCKET_SERVER_URL);
+
+      await new Promise<void>((resolve, reject) => {
+        socket.on("connect", () => {
+          console.log('Connected to WebSocket server')
+          resolve();
+        })
+
+        socket.on("error", (error) => {
+          console.error('WebSocket connection error:', error)
+          reject(error);
+        })
+      })
+      const jsonData = { message: data };
+      socket.emit('message', JSON.stringify(jsonData));
+
+      // const response: string = await new Promise<string>((resolve) => {
+      //   socket.on('message', (responseData: string) => {
+      //     console.log('Received message from WebSocket server:', responseData);
+      //     resolve(responseData)
+      //   });
+      // })
+      
+      return data
+
+    } catch (error) {
+      console.error('WebSocket operation failed:', error);
+      throw error;
+    }
+  }),
 
 })
