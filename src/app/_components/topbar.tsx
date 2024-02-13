99a7ea93287a06074a01b9d7d4f2745bf19e52cb
Petchx <petchxpanuphong@gmail.com>
2024-02-13 21:04:53 +0700

    feat: add topbar

---
+++ src/app/_components/topbar.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Link, Navbar, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@nextui-org/react'
+import { usePathname } from 'next/navigation'
+import React, { useState } from 'react'
+
+type Props = {}
+
+export default function Topbar({ }: Props) {
+    const pathname = usePathname()
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    return (
+        <Navbar maxWidth='full' className=' rounded-md shadow-small top-4'>
+            <NavbarMenuToggle
+                aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                className="lg:hidden"
+            />
+            <NavbarContent className="flex gap-4 font-bold">
+                <NavbarItem >
+                    {pathname == "/dashboard" && <div className=' flex items-center gap-2'>Overview</div>}
+                    {pathname == "/dashboard/students" && <div className=' flex items-center gap-2'>Students</div>}
+                    {pathname == "/dashboard/analytics" && <div className='flex items-center gap-2'>Analytics</div>}
+
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarMenu className='bg-[#f8f7fa]'>
+                <NavbarMenuItem className='mt-6'>
+                    <Link color='primary' className={`${pathname == '/dashboard' && 'font-bold'} w-full`} href='/dashboard' size="lg">Overview</Link>
+                    <Link color='primary' className={`${pathname == '/dashboard/students' && 'font-bold'} w-full`} href='/dashboard/students' size="lg">Students</Link>
+                    <Link color='primary' className={`${pathname == '/dashboard/analytics' && 'font-bold'} w-full`} href='/dashboard/analytics' size="lg">Analytics</Link>
+                </NavbarMenuItem>
+            </NavbarMenu>
+        </Navbar>
+    )
+}
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "../_components/sidebar";
 import { getServerAuthSession } from "~/server/auth";
 import { redirect } from "next/navigation";
+import Topbar from "../_components/topbar";
 
 export default async function Layout({
     children,
@@ -12,9 +13,16 @@ export default async function Layout({
     if (!session) return redirect("/api/auth/signin");
 
     return (
-        <main className="flex">
+
+        <main className='flex min-h-[100dvh]'>
             <Sidebar />
-            <div className="w-full p-6">{children}</div>
+            <div className='w-full bg-[#f8f7fa] p-4'>
+                <Topbar />
+                <div className="">
+                    {children}
+                </div>
+            </div>
+
         </main>
     );
 }
